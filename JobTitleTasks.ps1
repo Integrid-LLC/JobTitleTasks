9729d344eedc5dbe459443c12596ca9ce099cc90
David E. Lambert <david@amberanalytics.net>
2023-12-04 15:50:35 -0500

    revert extra params

--- JobTitleTasks.ps1
+++ JobTitleTasks.ps1
@@ -9,16 +9,10 @@ param (
     [string]$CertificateThumbprint,
 
     [Parameter(Mandatory = $true)]
-    [string]$CompanyName,
+    [int]$AutoTaskTicketId,
 
     [Parameter(Mandatory = $true)]
-    [string]$Domain,
-
-    [Parameter(Mandatory = $true)]
-    [string]$SpSiteName,
-
-    [Parameter(Mandatory = $true)]
-    [string]$SpListName,
+    [string]$AutoTaskTicketNumber,
 
     [Parameter(Mandatory = $true)]
     [string]$FirstName,
@@ -27,10 +21,16 @@ param (
     [string]$LastName,
 
     [Parameter(Mandatory = $true)]
-    [string[]]$Location,
+    [string]$UserPrincipalName,
 
     [Parameter(Mandatory = $true)]
     [string]$JobTitle,
+
+    [Parameter(Mandatory = $true)]
+    [string[]]$Location,
+
+    [Parameter(Mandatory = $true)]
+    [string[]]$LocationEmails,
 
     [Parameter(Mandatory = $false)]
     [string[]]$Equipment,
@@ -42,64 +42,10 @@ param (
     [string]$CreatedByDisplayName,
 
     [Parameter(Mandatory = $true)]
-    [string]$PasswordSender,
-
-    [Parameter(Mandatory = $true)]
     [string]$LicenseName,
 
     [Parameter(Mandatory = $true)]
-    [string]$ApprovalWebhookUrl,
-
-    [Parameter(Mandatory = $true)]
-    [string]$FollowUpAutomationAccount,
-
-    [Parameter(Mandatory = $true)]
-    [string]$FollowUpResourceGroup,
-
-    [Parameter(Mandatory = $true)]
-    [string]$FollowUpRunbook,
-
-    [Parameter(Mandatory = $true)]
-    [string]$UserPrincipalName,
-    
-    [Parameter(Mandatory = $true)]
-    [int]$AutoTaskCompanyId,
-
-    [Parameter(Mandatory = $true)]
-    [int]$HuduCompanyId,
-
-    [Parameter(Mandatory = $true)]
-    [string]$M365Location,
-
-    [Parameter(Mandatory = $true)]
-    [string]$AutoTaskLocationId,
-
-    [Parameter(Mandatory = $true)]
-    [string[]]$LocationEmails,
-
-    [Parameter(Mandatory = $true)]
-    [object]$UserData,
-
-    [Parameter(Mandatory = $true)]
-    [int]$AutoTaskTicketId,
-
-    [Parameter(Mandatory = $true)]
-    [string]$AutoTaskTicketNumber,
-
-    [Parameter(Mandatory = $true)]
-    [int]$AutoTaskContactId,
-
-    [Parameter(Mandatory = $true)]
-    [string]$HuduPasswordId,
-
-    [Parameter(Mandatory = $true)]
-    [bool]$OneTimeSecretSuccess,
-
-    [Parameter(Mandatory = $true)]
-    [obj]$LicenseData,
-
-    [Parameter(Mandatory = $true)]
-    [bool]$licenseApprovalRequired,
+    [string]$CompanyName,
 
     [Parameter(Mandatory = $true)]
     [bool]$AddPax8License
